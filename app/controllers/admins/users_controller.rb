550941d5cad597c5e4b990ec0992fd8d55946a45
“Ejima-we” <takumi941503@gmail.com>
2021-04-10 15:34:31 +0000

    管理者側会員編集機能作成

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,12 +1,20 @@
 class Admins::UsersController < ApplicationController
   
   def index
+    @users = User.all
   end
   
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admins_users_path
+    else
+      render :edit
+    end
   end
   
 end
