37205e10cc7142330b05d7bb67cdc53175931fad
“Ejima-we” <takumi941503@gmail.com>
2021-04-15 04:40:30 +0000

    コメント機能非同期化

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -6,29 +6,29 @@ def create
   @comment.user_id = current_user.id
   @comment.post_id = @post.id
   @comment.save
-  redirect_back(fallback_location: root_path)
+  render :index
 end
 
-def edit
-  @post = Post.find(params[:post_id])
-  @comment = Comment.find(params[:id])
-end
+# def edit
+#   @post = Post.find(params[:post_id])
+#   @comment = Comment.find(params[:id])
+# end
 
-def update
-  @post = Post.find(params[:post_id])
-  @comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
-  if @comment.update(comment_params)
-    redirect_to post_path(@post.id)
-  else
-    render :show
-  end
-end
+# def update
+#   @post = Post.find(params[:post_id])
+#   @comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+#   if @comment.update(comment_params)
+#     redirect_to post_path(@post.id)
+#   else
+#     render :show
+#   end
+# end
 
 def destroy
   @post = Post.find(params[:post_id])
   @comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
   @comment.destroy
-  redirect_to post_path(@post.id)
+  render :index
 end
 
 private
