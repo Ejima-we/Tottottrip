1f437a3e44e62769a223459dd3267fb54e4e1e97
“Ejima-we” <takumi941503@gmail.com>
2021-04-23 08:49:50 +0000

    kaminari表示数変更

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -5,8 +5,8 @@ class Users::HomesController < ApplicationController
 
   def top
     @genres = Genre.all
-    @posts_rank = Post.find(Favorite.group(:post_id).order("count(post_id)desc").limit(4).pluck(:post_id))
-    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(4)
+    @posts_rank = Post.find(Favorite.group(:post_id).order("count(post_id)desc").limit(9).pluck(:post_id))
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(9)
   end
 
   def inquiries
